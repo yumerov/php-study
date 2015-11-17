91f0f0ee96c3a09bc9046a4a60deda6be8e67c13
Levent Yumerov <levent.yumerov@flatrocktech.com>
2015-11-17 10:15:34 +0200

    masic memcached example - added

---
+++ cache/memcached/fibonacci/memcached.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!class_exists('Memcached')) {
+  die('This demo requires installed Memcached module.');
+}
+
+$cache = new Memcached();
+$cache->addServer("localhost", 11211);
+
+function fibonacci($n, $cache) {
+  $key = 'fib_' . $n;
+  $cachedValue = $cache->get($key);
+  if ($cachedValue !== FALSE) {
+    $return = $cachedValue;
+  } else {
+    if ($n < 0) {
+      $return = NULL;
+    } elseif ($n === 0) {
+      $return = 0;
+    } elseif ($n === 1 || $n === 2) {
+      $return = 1;
+    } else {
+      $return = fibonacci($n-1, $cache) + fibonacci($n-2, $cache);
+    }
+  }
+
+  $cache->set($key, $return);
+
+  return $return;
+}
+
+echo fibonacci(135, $cache);
