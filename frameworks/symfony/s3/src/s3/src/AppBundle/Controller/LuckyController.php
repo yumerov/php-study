6973d53af17cda863d620a049ee836271e40be63
Levent Yumerov <yumerov.levent@gmail.com>
2015-12-15 10:01:05 +0200

    Symfony: s3 -> first page

---
+++ frameworks/symfony/s3/src/s3/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+  /**
+  * @Route("/lucky/number")
+  */
+  public function numberAction()
+  {
+    $number = rand(0, 100);
+
+    return new Response("<html><body>lucky number: $number</body></html>");
+  }
+}
