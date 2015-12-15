1026bfa1cccd378b883c82fcd653ec58db716a16
Levent Yumerov <yumerov.levent@gmail.com>
2015-12-15 10:11:56 +0200

    Symfony: s3 -> json response

--- frameworks/symfony/s3/src/s3/src/AppBundle/Controller/LuckyController.php
+++ frameworks/symfony/s3/src/s3/src/AppBundle/Controller/LuckyController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LuckyController
 {
@@ -15,5 +16,15 @@ class LuckyController
     $number = rand(0, 100);
 
     return new Response("<html><body>lucky number: $number</body></html>");
+  }
+
+  /**
+   * @Route("/api/lucky/number")
+   */
+  public function apiNumberAction()
+  {
+    $data = ['lucky_number' => rand(1, 100)];
+
+    return new JsonResponse($data);
   }
 }
