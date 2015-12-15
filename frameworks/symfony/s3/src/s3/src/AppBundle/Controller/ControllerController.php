e84c627635382d7184a82b3ec1856ff87cbf11b5
Levent Yumerov <yumerov.levent@gmail.com>
2015-12-15 11:22:15 +0200

    Symfony: s3 -> error page

--- frameworks/symfony/s3/src/s3/src/AppBundle/Controller/ControllerController.php
+++ frameworks/symfony/s3/src/s3/src/AppBundle/Controller/ControllerController.php
@@ -10,12 +10,20 @@ use Symfony\Component\HttpFoundation\Request;
 class ControllerController extends Controller
 {
 
-	/**
-	 * @Route("/controller/param")
-	 */
-	public function paramAction(Request $request)
-	{
-		$page = $request->query->get('page', 1);
-		return new Response("<html><body><h1>page: $page</h1></body></html>");
-	}
+  /**
+   * @Route("/controller/param")
+   */
+  public function paramAction(Request $request)
+  {
+    $page = $request->query->get('page', 1);
+    return new Response("<html><body><h1>page: $page</h1></body></html>");
+  }
+  
+  /**
+   * @Route("/controller/error")
+   */
+  public function errorAction()
+  {
+    throw $this->createNotFoundException("just an error page");
+  }
 }
