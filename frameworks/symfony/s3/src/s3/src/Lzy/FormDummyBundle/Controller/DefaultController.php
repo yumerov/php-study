11cf9d0afc204bea9e5a935a023c764957cd68c5
Levent Yumerov <yumerov.levent@gmail.com>
2015-12-22 11:02:41 +0200

    Symfony: s3 -> basic form handling

--- frameworks/symfony/s3/src/s3/src/Lzy/FormDummyBundle/Controller/DefaultController.php
+++ frameworks/symfony/s3/src/s3/src/Lzy/FormDummyBundle/Controller/DefaultController.php
@@ -8,10 +8,14 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller{
 
-  public function newAction(Request $request) {
+  /**
+   * @return \Symfony\Component\Form\FormBuilder
+   */
+  private function _generateForm() {
     $task = new Task();
     $task->setTask("change the world")->setDueDate(new \DateTime('tomorrow'));
     
@@ -21,6 +25,27 @@ class DefaultController extends Controller{
       ->add('save', SubmitType::class, ['label' => 'Create task'])
       ->getForm()
     ;
+    
+    return $form;
+  }
+  
+  public function newAction(Request $request) {
+    $form = $this->_generateForm();
+    
+    return $this->render(
+      "FormDummyBundle:Default:new.html.twig", ['form' => $form->createView()]);
+  }
+  
+  
+  public function handleAction(Request $request) {
+    $form = $this->_generateForm();
+    
+    $form->handleRequest($request);
+    
+    if ($form->isSubmitted() && $form->isValid())
+    {
+      return new Response("the form is valid");
+    }
     
     return $this->render(
       "FormDummyBundle:Default:new.html.twig", ['form' => $form->createView()]);
