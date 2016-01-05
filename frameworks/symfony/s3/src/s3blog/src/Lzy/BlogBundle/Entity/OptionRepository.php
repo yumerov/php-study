b4f0075c3c251e7f6354ce973b4cc96600114292
Levent Yumerov <yumerov.levent@gmail.com>
2016-01-05 14:21:56 +0200

    Symfony 3 blog - moved important strings to class constants

--- frameworks/symfony/s3/src/s3blog/src/Lzy/BlogBundle/Entity/OptionRepository.php
+++ frameworks/symfony/s3/src/s3blog/src/Lzy/BlogBundle/Entity/OptionRepository.php
@@ -4,79 +4,138 @@ namespace Lzy\BlogBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Option Repository class
+ */
 class OptionRepository extends EntityRepository {
 
+  /**
+   * Default posts per page value
+   * 
+   * @var integer
+   */
   const PER_PAGE = 2;
 
+  /**
+   * Posts per page database option key
+   * 
+   * @var string
+   */
+  const PER_PAGE_KEY = 'posts.per_page';
+
+  /**
+   * Default blog title
+   * 
+   * @var string
+   */
+  const BLOG_TITLE = '';
+
+  /**
+   * Blog title database option key
+   * 
+   * @var string
+   */
+  const BLOG_TITLE_KEY = 'blog.title';
+
+  /**
+   * Default blog description
+   * 
+   * @var string
+   */
+  const BLOG_DESCRIPTION = '';
+
+  /**
+   * Blog description database option key
+   * 
+   * @var string
+   */
+  const BLOG_DESCRIPTION_KEY = 'blog.description';
+
+  /**
+   * Default posts author
+   * 
+   * @var string
+   */
+  const POSTS_AUTHOR = 'lzy';
+
+  /**
+   * Posts author database option key
+   * 
+   * @var string
+   */
+  const POSTS_AUTHOR_KEY = 'posts.author';
+
+  /**
+   * Default date format
+   * 
+   * @var string
+   */
+  const DATE_FORMAT = "F d, Y";
+
+  /**
+   * Date format databse option key
+   * 
+   * @var string
+   */
+  const DATE_FORMAT_KEY = "posts.date_format";
+
+  /**
+   * Returns blog title stored in the database or default value
+   *
+   * @var string
+   */
   public function getBlogTitle() {
-    $record = $this->findOneByKey('blog.title');
-
-    if ($record) {
-      $title = $record->getValue();
-    } else {
-      $title = '';
-    }
-
+    $record = $this->findOneByKey(self::BLOG_TITLE_KEY);
+    $title = ($record) ? $record->getValue() : self::BLOG_TITLE;
     return $title;
   }
 
+  /**
+   * Returns blog description stored in the database or default value
+   *
+   * @var string
+   */
   public function getBlogDescription() {
-    $record = $this->findOneByKey('blog.description');
-
-    if ($record) {
-      $description = $record->getValue();
-    } else {
-      $description = '';
-    }
-
+    $record = $this->findOneByKey(self::BLOG_DESCRIPTION_KEY);
+    $description = ($record) ? $record->getValue() : self::BLOG_DESCRIPTION;
     return $description;
   }
 
+  /**
+   * Returns posts' author stored in the database or default value
+   *
+   * @var string
+   */
   public function getPostsAuthor() {
-    $record = $this->findOneByKey('posts.author');
-
-    if ($record) {
-      $author = $record->getValue();
-    } else {
-      $author = '';
-    }
-
+    $record = $this->findOneByKey(self::POSTS_AUTHOR_KEY);
+    $author = ($record) ? $record->getValue() : self::POSTS_AUTHOR;
     return $author;
   }
 
   /**
+   * Returns posts' date format stored in the database or default value
    * 
-   * @return integer
+   * @return string
    */
   public function getPostsDateFormat() {
-    $record = $this->findOneByKey('posts.date_format');
-
-    if ($record) {
-      $format = $record->getValue();
-    } else {
-      $format = "F d, Y";
-    }
-
+    $record = $this->findOneByKey(self::DATE_FORMAT_KEY);
+    $format = ($record) ? $record->getValue() : self::DATE_FORMAT;
     return $format;
   }
 
   /**
+   * Returns posts per page stored in the database or default value
    * 
    * @return integer
    */
   public function getPerPage() {
-    $record = $this->findOneByKey('posts.per_page');
-
-    if ($record) {
-      $perPage = $record->getValue();
-    } else {
-      $perPage = self::PER_PAGE;
-    }
-
+    $record = $this->findOneByKey(self::PER_PAGE_KEY);
+    $perPage = ($record) ? $record->getValue() : self::PER_PAGE;
     return $perPage;
   }
 
   /**
+   * Returns general data used in the templates
    * 
    * @return Array
    */
--- frameworks/symfony/s3/src/s3blog/src/Lzy/BlogBundle/Service/Data.php
+++ frameworks/symfony/s3/src/s3blog/src/Lzy/BlogBundle/Service/Data.php
@@ -3,27 +3,37 @@
 namespace Lzy\BlogBundle\Service;
 
 use Doctrine\ORM\EntityManager;
-use Lzy\BlogBundle\Entity\OptionRepository;
-use Lzy\BlogBundle\Entity\PostRepository;
 
+/**
+ * Blog Data service class
+ */
 class Data {
 
+  /**
+   *
+   * @var Doctrine\ORM\EntityManager 
+   */
   protected $_em;
 
+  /**
+   * @param EntityManager $entityManager
+   */
   public function __construct(EntityManager $entityManager) {
     $this->_em = $entityManager;
   }
 
   /**
+   * Returns data needed for populating post list page template
    * 
+   * @param integer $page
    * @return Array
    */
   public function getPostListData($page) {
 
-    /** @var PostRepository */
+    /** @var Lzy\BlogBundle\Entity\OptionRepository */
     $postRepository = $this->_em->getRepository('LzyBlogBundle:Post');
 
-    /** @var OptionRepository */
+    /** @var Lzy\BlogBundle\Entity\PostRepository; */
     $optionRepository = $this->_em->getRepository('LzyBlogBundle:Option');
 
     $perPage = $optionRepository->getPerPage();
