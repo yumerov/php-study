1cbacbbc11b6531714dc8252e883cffadca099ec
Levent Yumerov <yumerov.levent@gmail.com>
2016-01-11 16:02:39 +0200

    Frameworks:Symfony28:Cooking book:event subscriber

---
+++ frameworks/symfony/cooking-symfony/src/cooking-symfony/src/Lzy/EventBundle/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lzy\EventBundle\EventSubscriber;
+
+use Doctrine\Common\EventSubscriber;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ExceptionSubscriber implements \Symfony\Component\EventDispatcher\EventSubscriberInterface {
+
+  public static function getSubscribedEvents() {
+    return [
+      'kernel.exception' => [
+        ['processException', 10],
+        ['logException', 0],
+        ['notifyException', -10],
+      ]
+    ];
+  }
+
+  public function processException(GetResponseForExceptionEvent $event) {
+    // ...
+  }
+
+  public function logException(GetResponseForExceptionEvent $event) {
+    // ...
+  }
+
+  public function notifyException(GetResponseForExceptionEvent $event) {
+    // ...
+  }
+
+}
