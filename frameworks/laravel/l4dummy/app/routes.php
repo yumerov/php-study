77721747c7fc52f36a01847c46ed9af58f3aa970
Levent Yumerov <yumerov.levent@gmail.com>
2016-04-07 09:42:06 +0300

    Eloquent Basics
    See more @ https://laracasts.com/series/laravel-from-scratch/episodes/5?autoplay=true

--- frameworks/laravel/l4dummy/app/routes.php
+++ frameworks/laravel/l4dummy/app/routes.php
@@ -5,3 +5,36 @@ Route::get('/users', function () {
 
     return $users;
 });
+
+Route::get('/users/orm', function () {
+    $users = User::all();
+
+    return $users;
+});
+
+Route::get('/users/orm/find', function () {
+    $users = User::find(2);
+
+    return $users;
+});
+
+Route::get('/users/orm/create', function () {
+    $user = new User();
+    $user->username = 'ramm';
+    $user->password = Hash::make('stein');
+    $user->save();
+
+    return User::all();
+});
+
+Route::get('/users/orm/update', function () {
+    $user = User::find(2);
+    $user->username = 'updated::' .rand(1, 10);
+    $user->save();
+
+    return User::all();
+});
+
+Route::get('/users/orm/order', function () {
+    return User::orderBy('username', 'asc')->get();
+});
