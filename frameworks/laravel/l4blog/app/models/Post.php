d7714d5ec1e19445344cf411bc73b875daecada6
Levent Yumerov <yumerov.levent@gmail.com>
2016-04-13 15:27:39 +0300

    Frameworks:Laravel:l4blog - soft delete of Post

--- frameworks/laravel/l4blog/app/models/Post.php
+++ frameworks/laravel/l4blog/app/models/Post.php
@@ -2,6 +2,10 @@
 
 class Post extends \Eloquent {
 
+	use SoftDeletingTrait;
+
+	protected $dates = ['deleted_at'];
+
   public static $rules = [
     'title' => 'required|min:3',
     'body' => 'required|min:3',
